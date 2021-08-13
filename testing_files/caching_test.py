c1b797b17c114fc552a58c476b4dd07074f39bbb
thomaslee01 <t97lee@uwaterloo.ca>
2021-08-13 17:08:08 -0400

    added testing .ipynb files

---
+++ testing_files/caching_test.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import time
+
+@st.cache(suppress_st_warning=True)
+def expensive_computation(a, b):
+    st.write("Cache miss: expensive_computation(", a, ",", b, ") ran")
+    time.sleep(2)  # This makes the function take 2s to run
+    return a * b
+
+a = 2
+b = st.slider("Pick a number", 0, 10)  # 👈 Changed this
+res = expensive_computation(a, b)
